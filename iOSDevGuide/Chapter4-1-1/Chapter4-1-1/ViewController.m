f03a8e9ca9b077d1277c9f50e538be137f2df7d2
gameboys84 <gameboys84@163.com>
2017-09-23 23:37:38 +0800

    加入UIScrollView，实现键盘出现时，如果虚拟键盘会遮挡输入控件，会自动向下滚动键盘高度的距离，如果不遮挡，这不会滚动。（updateScorllView）

--- iOSDevGuide/Chapter4-1-1/Chapter4-1-1/ViewController.m
+++ iOSDevGuide/Chapter4-1-1/Chapter4-1-1/ViewController.m
@@ -10,7 +10,11 @@
 
 @interface ViewController () <UITextFieldDelegate, UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
+@property UIView* activeField;
+@property CGFloat keyboardHeight;
+@property CGFloat scrollViewHeight;
 @end
 
 @implementation ViewController
@@ -18,6 +22,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTapTouched:)];
+    tapGesture.numberOfTapsRequired = 1;
+    
+    // 必须为当前view开启交互，默认是关闭的
+    [self.view setUserInteractionEnabled:true];
+    [self.view addGestureRecognizer:tapGesture];
+    
+    //self.scrollView = (UIScrollView*)self.view;
+    self.scrollViewHeight = [UIScreen mainScreen].bounds.size.height;
+    NSLog(@"height: viewHeight=%@", NSStringFromCGRect(self.view.frame));
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,17 +46,46 @@
     self.label.text = [NSString stringWithFormat:@"%d Hello World!", rand()];
 }
 
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    self.activeField = textField;
+    [self updateScorllView:YES];
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField
+{
+    self.activeField = NULL;
+}
+
+-(void)textViewDidBeginEditing:(UITextView *)textView
+{
+    self.activeField = textView;
+    [self updateScorllView:YES];
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView
+{
+    self.activeField = NULL;
+}
+
+- (void)onTapTouched:(UITapGestureRecognizer*)sender
+{
+    //NSLog(@"onTapTouched:%ld", sender.state);
+    
+    [self.view endEditing:true];
+}
+
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
 {
-    NSLog(@"txt:%@", text);
+    //NSLog(@"txt:%@", text);
     
-    if ([text isEqualToString:@"\n"])
-    //if ([text isEqualToString:UIKeyInputEscape])
-    {
-        //NSLog(@"return input");
-        [textView resignFirstResponder];
-        return NO;
-    }
+//    if ([text isEqualToString:@"\n"])
+//    //if ([text isEqualToString:UIKeyInputEscape])
+//    {
+//        //NSLog(@"return input");
+//        [textView resignFirstResponder];
+//        return NO;
+//    }
     
     return YES;
 }
@@ -65,16 +109,49 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidHideNotification object:nil];
 }
 
+- (void)updateScorllView:(BOOL)openKeyboard {
+    if (openKeyboard)
+    {
+        UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, self.keyboardHeight, 0.0);
+        [self.scrollView setContentInset:contentInsets];
+        [self.scrollView setScrollIndicatorInsets:contentInsets];
+        
+        CGRect aRect = self.view.frame;
+        aRect.size.height -= self.keyboardHeight;
+        // 需要滚动时才会滚动
+        if (!CGRectContainsPoint(aRect, self.activeField.frame.origin))
+            [self.scrollView setContentOffset:CGPointMake(0, self.keyboardHeight) animated:YES];
+    }
+    else
+    {
+        UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+        self.scrollView.contentInset = contentInsets;
+        self.scrollView.scrollIndicatorInsets = contentInsets;
+        [self.scrollView setContentOffset:CGPointMake(0, 0) animated:YES];
+    }
+}
+
 -(void)keyboardDidShow:(NSNotification*) notify
 {
-    NSLog(@"keyboard show");
-    //self.view.frame.origin.y =  216;
+    //NSLog(@"keyboard show");
+    CGSize sizeKeyboardBegin = [[notify.userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    CGSize sizeKeyboardEnd = [[notify.userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size;
+    NSLog(@"size: begin=%@, end=%@", NSStringFromCGSize(sizeKeyboardBegin), NSStringFromCGSize(sizeKeyboardEnd));
+    
+    self.keyboardHeight = sizeKeyboardEnd.height; // 271
+
+    [self updateScorllView:true];
+    
+    NSLog(@"SHOW viewHeight:%@, scorllHeight:%@, keyboard:%.2f", NSStringFromCGRect(self.scrollView.frame), NSStringFromCGRect(self.view.frame), self.keyboardHeight);
 }
 
 -(void)keyboardDidHide:(NSNotification*) notify
 {
-    NSLog(@"keyboard hide");
-    
+    //NSLog(@"keyboard hide");
+
+    [self updateScorllView:NO];
+
+    NSLog(@"HIDE viewHeight:%@, scorllHeight:%@, keyboard:%.2f", NSStringFromCGRect(self.scrollView.frame), NSStringFromCGRect(self.view.frame), self.keyboardHeight);
 }
 
 @end
--- iOSDevGuide/iOS开发指南.md
+++ iOSDevGuide/iOS开发指南.md
@@ -5,6 +5,7 @@ Chapter4  UIView与视图
   1-1: 常规实现
   
       Storyboard,IB（Interface Build）实现，Label,Button, TextField（单行）,TextView（多行）事件处理，键盘出现和消失（resignFirstResponder）
+      加入UIScrollView，实现键盘出现时，如果虚拟键盘会遮挡输入控件，会自动向下滚动键盘高度的距离，如果不遮挡，这不会滚动。（updateScorllView）
        
   1-2: xib实现RootViewController加载
   
